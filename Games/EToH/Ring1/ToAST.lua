9032a1dc8a93bc484f601b7c6f8cd1b58f1bd078
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-05-26 17:52:08 +0800

    Update ToAST.lua

--- Games/EToH/Ring1/ToAST.lua
+++ Games/EToH/Ring1/ToAST.lua
@@ -1,1 +1,18 @@
-
+return function()
+    return {
+        workspace.Towers.ToAST.Obby:GetChildren()[67],
+        workspace.Towers.ToAST.Obby:GetChildren()[73],
+        workspace.Towers.ToAST.Obby:GetChildren()[72],
+        workspace.Towers.ToAST.Obby.Bridge:GetChildren()[22],
+        workspace.Towers.ToAST.Obby:GetChildren()[75],
+        workspace.Towers.ToAST.Obby:GetChildren()[112],
+        workspace.Towers.ToAST.Obby:GetChildren()[105],
+        workspace.Towers.ToAST.Obby:GetChildren()[108],
+        workspace.Towers.ToAST.Obby:GetChildren()[115],
+        workspace.Towers.ToAST.Obby:GetChildren()[114],
+        workspace.Towers.ToAST.Obby:GetChildren()[161],
+        workspace.Towers.ToAST.Obby:GetChildren()[157],
+        workspace.Towers.ToAST.Obby:GetChildren()[150],
+        workspace.Towers.ToAST.Obby:GetChildren()[155],
+    }
+end
