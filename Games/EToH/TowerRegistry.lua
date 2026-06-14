b431639d5640f7083d701e0a801f74e23c832755
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-06-14 11:57:35 +0800

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -74,5 +74,6 @@ return {
     },
     TowerRush = {
         { name = "R1TR", category = "Ring1", suggestedTime = { min = "39", sec = "0" }, isTowerRush = true, },
+        { name = "R14MTR", category = "Ring5", suggestedTime = { min = "0", sec = "50" }, isTowerRush = true, },
     },
 }
