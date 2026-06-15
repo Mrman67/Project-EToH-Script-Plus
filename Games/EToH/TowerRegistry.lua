dfd2360235d72e6242e0703fae466e10d6624e49
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-06-15 19:58:43 +0800

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -76,6 +76,7 @@ return {
         { name = "ToOC",  category = "Ring5", suggestedTime = { min = "3", sec = "0" } },
         { name = "ToFF",  category = "Ring5", suggestedTime = { min = "3", sec = "0" } },
         { name = "ToTH",  category = "Ring5", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToGaH",  category = "Ring5", suggestedTime = { min = "3", sec = "0" } },
         { name = "CoCR",  category = "Ring5", suggestedTime = { min = "3", sec = "0" } },
         { name = "ToIB",  category = "Ring5", suggestedTime = { min = "3", sec = "0" } },
         { name = "ToFN",  category = "Ring5", suggestedTime = { min = "3", sec = "0" } },
