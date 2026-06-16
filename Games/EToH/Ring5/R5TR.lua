26b47385e655a3d5f0a6e3a3d0e2e689c3970747
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-06-16 18:57:39 +0800

    Create R5TR.lua

---
+++ Games/EToH/Ring5/R5TR.lua
@@ -0,0 +1,15 @@
+return function()
+    return {
+        "ToNI",
+        "ToTR",
+        "ToRS",
+        "ToDM",
+        "ToOC",
+        "ToFF",
+        "ToEA",
+        "ToTH",
+        "ToGaH",
+        "ToFO",
+        "CoCR"
+    }
+end
