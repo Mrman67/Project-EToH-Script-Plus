246196c898e8717f6d64f838ebc6591952329371
cslp1 <wrerfsdh5@gmail.com>
2026-06-23 18:06:58 +0200

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -37,6 +37,7 @@ return {
         { name = "ToHA",  category = "Ring9", suggestedTime = { min = "3", sec = "0" } },
         { name = "ToBT",  category = "Ring9", suggestedTime = { min = "3", sec = "0" } },
         { name = "ToCA",  category = "Ring9", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToNS",  category = "Ring9", suggestedTime = { min = "3", sec = "0" } },
         --Zone 1
         { name = "ToTL",  category = "Zone1", suggestedTime = { min = "1", sec = "0" } },
         --Zone 2
