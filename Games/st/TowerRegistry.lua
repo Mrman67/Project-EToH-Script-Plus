3893493e37e625ce9e82eee7a2d7bb244a2f88ba
cslp1 <wrerfsdh5@gmail.com>
2026-06-23 12:07:29 +0200

    Create TowerRegistry.lua

---
+++ Games/st/TowerRegistry.lua
@@ -0,0 +1,6 @@
+return {
+    Categories = {
+        Location4 = 129965911879791,
+    },
+    Towers = {
+        { name = "c",      category = "Location4", suggestedTime = { min = "3", sec = "0" } },
