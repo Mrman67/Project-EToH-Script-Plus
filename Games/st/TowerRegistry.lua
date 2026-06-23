be025313812ef442fc63bf18fb5ad1e928836c0f
cslp1 <wrerfsdh5@gmail.com>
2026-06-23 12:19:02 +0200

    Update TowerRegistry.lua

--- Games/st/TowerRegistry.lua
+++ Games/st/TowerRegistry.lua
@@ -3,4 +3,7 @@ return {
         Location4 = 129965911879791,
     },
     Towers = {
-        { name = "c",      category = "Location4", suggestedTime = { min = "3", sec = "0" } },
+        { name = "c", category = "Location4", suggestedTime = { min = "3", sec = "0" } },
+    },
+    TowerRush = {},
+}
